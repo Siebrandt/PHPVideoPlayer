2ec15505897829f180aac77b43eee5f715a8e864
Siebrandt <45029232+Siebrandt@users.noreply.github.com>
2019-05-22 08:52:49 +0200

    Database SQL script and schema added
    Database SQL script and schema added

---
+++ SQL_PHPPlayer.sql
@@ -0,0 +1,69 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema PHPVideoPlayer
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema PHPVideoPlayer
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `PHPVideoPlayer` DEFAULT CHARACTER SET utf8 ;
+USE `PHPVideoPlayer` ;
+
+-- -----------------------------------------------------
+-- Table `PHPVideoPlayer`.`playlist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PHPVideoPlayer`.`playlist` ;
+
+CREATE TABLE IF NOT EXISTS `PHPVideoPlayer`.`playlist` (
+  `pid` INT NOT NULL,
+  `plname` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`pid`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `PHPVideoPlayer`.`video`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PHPVideoPlayer`.`video` ;
+
+CREATE TABLE IF NOT EXISTS `PHPVideoPlayer`.`video` (
+  `vid` INT NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `video` LONGBLOB NOT NULL,
+  `thumbnail` LONGBLOB NOT NULL,
+  `duration` INT NOT NULL,
+  PRIMARY KEY (`vid`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `PHPVideoPlayer`.`playlist_has_video`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PHPVideoPlayer`.`playlist_has_video` ;
+
+CREATE TABLE IF NOT EXISTS `PHPVideoPlayer`.`playlist_has_video` (
+  `pid` INT NOT NULL,
+  `vid` INT NOT NULL,
+  INDEX `fk_playlist_has_video_playlist_idx` (`pid` ASC),
+  INDEX `fk_playlist_has_video_video1_idx` (`vid` ASC),
+  CONSTRAINT `fk_playlist_has_video_playlist`
+    FOREIGN KEY (`pid`)
+    REFERENCES `PHPVideoPlayer`.`playlist` (`pid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_playlist_has_video_video1`
+    FOREIGN KEY (`vid`)
+    REFERENCES `PHPVideoPlayer`.`video` (`vid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
